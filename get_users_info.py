b323609c7b585e69ad2b354c953857c3edac2365
gcmurillo <gcmurillo@outlook.com>
2019-08-08 14:22:22 -0500

    get located data

--- get_users_info.py
+++ get_users_info.py
@@ -2,7 +2,7 @@ import os
 import json
 import sys
 
-rootdir = '/media/geancarlo/OS_Install/Users/geancarlo/Desktop/tweets'  # carpeta con subcarpetas de tweets
+rootdir = '/media/geancarlo/OS_Install/Users/geancarlo/Desktop/tweets/cuenca_1'  # carpeta con subcarpetas de tweets
 '''
 > tweets
     > guayaquil_1
@@ -14,10 +14,12 @@ non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 
 users = []
 file = open('users_dataset.csv', 'w')
-file.write('screen_name,following,followers,favourites_count\n')
+file.write('screen_name,following,followers,favourites_count,verified\n')
 
 tweets_file = open('tweets_dataset.csv', 'w', encoding='utf-8-sig')
-tweets_file.write('screen_name,tweet,favorite_count,retweet_count,reply_to,n_user_mentions,n_hashtags,n_urls,user_mentions,hashtags\n')
+tweets_file.write('screen_name,tweet,favorite_count,retweet_count,reply_to,n_user_mentions,n_hashtags,n_urls,user_mentions,hashtags,place\n')
+
+cities = ['Guayaquil', 'Quito', 'Cuenca']
 
 for root, dirs, files in os.walk(rootdir):
     for name in files:
@@ -29,24 +31,27 @@ for root, dirs, files in os.walk(rootdir):
             # get tweets from the json file
             tweets = data['data']
             for t in tweets:
-                user = t['user']['screen_name']
-                if user not in users:  # for user dataset
-                    users.append(user)
-                    file.write(user + ',' + str(t['user']['friends_count']) + ',' +  str(t['user']['followers_count']) + ',' +
-                                str(t['user']['favourites_count']) + '\n')
+                if t['place']['name'] in cities:
+                    user = t['user']['screen_name']
+                    if user not in users:  # for user dataset
+                        users.append(user)
+                        file.write(user + ',' + str(t['user']['friends_count']) + ',' +  str(t['user']['followers_count']) + ',' +
+                                    str(t['user']['favourites_count']) + ',' + str(t['user']['verified']) + '\n')
 
-                if 'extended_tweet' in t.keys():  # for tweets dataset
-                    tweet = str('\"' + t['extended_tweet']['full_text'] + '\"').replace('\n', ' ').replace(',', ';').encode('utf-8')
-                else:
-                    tweet = str('\"' + t['text'] + '\"').replace('\n', ' ').replace(',', ';').encode('utf-8')
-                reply = t['in_reply_to_screen_name'] if t['in_reply_to_screen_name'] is not None else ''
-                user_mentioned = ';'.join(i['screen_name'] for i in t['entities']['user_mentions'])
-                hashtags = ';'.join(i['text'] for i in t['entities']['hashtags'])
-                tweets_file.write(user + ',' + tweet.decode('utf-8') + ',' + str(t['favorite_count']) + ',' + str(t['retweet_count']) + ',' + reply + ',' + 
-                str(len(t['entities']['user_mentions'])) + ',' + str(len(t['entities']['hashtags'])) + ',' + str(len(t['entities']['urls'])) + ',"' + 
-                user_mentioned + '","' + hashtags + '"' '\n')
+                    if 'extended_tweet' in t.keys():  # for tweets dataset
+                        tweet = str('\"' + t['extended_tweet']['full_text'] + '\"').replace('\n', ' ').replace(',', ';').encode('utf-8')
+                    else:
+                        tweet = str('\"' + t['text'] + '\"').replace('\n', ' ').replace(',', ';').encode('utf-8')
+                    reply = t['in_reply_to_screen_name'] if t['in_reply_to_screen_name'] is not None else ''
+                    user_mentioned = ';'.join(i['screen_name'] for i in t['entities']['user_mentions'])
+                    hashtags = ';'.join(i['text'] for i in t['entities']['hashtags'])
+                    tweets_file.write(user + ',' + tweet.decode('utf-8') + ',' + str(t['favorite_count']) + ',' + str(t['retweet_count']) + ',' + reply + ',' + 
+                    str(len(t['entities']['user_mentions'])) + ',' + str(len(t['entities']['hashtags'])) + ',' + str(len(t['entities']['urls'])) + ',"' + 
+                    user_mentioned + '","' + hashtags + '",' +  t['place']['name'] + '\n')
+                    n_tweets += 1
             print('file_name: ', name, '| tweets:', len(tweets))
             print('users:', len(users), '| tweets:', n_tweets)  
 
+print('Complete')
 file.close()
 tweets_file.close()
